78313a7d8b2fbf9832e5d4255f18a9ea87bbe0c3
Youngblutt Erin <eyoungblutt1@woolworths.com.au>
2022-07-25 13:04:01 +1000

    dded clear all function

--- clearFields.js
+++ clearFields.js
@@ -1,3 +1,4 @@
 export let clearFields = () => {
   document.getElementById("pokemonCard").innerHTML = "";
 }
+
--- pokemonAPI.js
+++ pokemonAPI.js
@@ -10,13 +10,38 @@ document.getElementById('searchButton').addEventListener('click', async () => {
   // will currently accept id number or name of pokemon with all lower case - can not display or search multiple pokemon.
 let response = await fetch (`https://pokeapi.co/api/v2/pokemon/${searchQuery.value.toLowerCase()}`);
 let data = await response.json();
-let newCard = document.createElement("div");
-document.getElementById("body").appendChild(newCard);
+
 displayPokemonImage(data);
 displayPokemonName(data);
 displayPokemonAbilities(data);
 displayPokemonStats(data);
+
+
+
 })
+
+let clearAll = document.getElementById("clearAll");
+clearAll.addEventListener('click', function () {
+  clearFields();
+} )
+
+
+//have option to sve searches
+//save button that saves searches
+//saved into an array of objects? 
+// have class that constructs a new object from the constructor when the save button is clicked. have the data from the functions save into the constructor and then you can view the array??
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
