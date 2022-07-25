e88084590e8f54dc671c4c3d12fdd02c0edd41b1
Youngblutt Erin <eyoungblutt1@woolworths.com.au>
2022-07-25 17:08:12 +1000

    added some error handling

--- clearFields.js
+++ clearFields.js
@@ -1,4 +1,5 @@
 export let clearFields = () => {
   document.getElementById("pokemonCard").innerHTML = "";
+  document.getElementById("errorMessage").innerText = "";
 }
 
--- pokemonAPI.js
+++ pokemonAPI.js
@@ -7,22 +7,21 @@ import { clearFields } from './clearFields.js';
 let searchQuery = document.getElementById("search");
 
 document.getElementById('searchButton').addEventListener('click', async () => {
-  // will currently accept id number or name of pokemon with all lower case - can not display or search multiple pokemon.
+
 let response = await fetch (`https://pokeapi.co/api/v2/pokemon/${searchQuery.value.toLowerCase()}`);
+
+if(response.ok === true){
 let data = await response.json();
+
+document.getElementById("errorMessage").innerText = "";
 displayPokemonImage(data);
 displayPokemonName(data);
 displayPokemonAbilities(data);
 displayPokemonStats(data);
 
-
-//localStorage.setItem("image", JSON.stringify(displayPokemonImage(data)));
-// localStorage.setItem("name", JSON.stringify(displayPokemonName(data)));
-// localStorage.setItem("abilities", JSON.stringify(displayPokemonAbilities(data)));
-// localStorage.setItem("statistics", JSON.stringify(displayPokemonStats(data)));
-
-
-console.log(localStorage);
+}
+else {
+  document.getElementById("errorMessage").innerText = "Oops! Please double check your entry is a pokemon name or index number and try again."};
 
 })
 
