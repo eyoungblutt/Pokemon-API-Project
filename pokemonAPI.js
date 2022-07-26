30c6ecbe0f1b5f75310e55f02a3d623b410c28cb
Youngblutt Erin <eyoungblutt1@woolworths.com.au>
2022-07-26 13:08:32 +1000

    working on loal storage

--- pokemonAPI.js
+++ pokemonAPI.js
@@ -12,12 +12,34 @@ let response = await fetch (`https://pokeapi.co/api/v2/pokemon/${searchQuery.val
 
 if(response.ok === true){
 let data = await response.json();
-
 document.getElementById("errorMessage").innerText = "";
 displayPokemonImage(data);
 displayPokemonName(data);
 displayPokemonAbilities(data);
 displayPokemonStats(data);
+
+
+//local storage attempt
+
+let pokemonData = {
+  image: data.sprites.front_default,
+  firstName: data.name,
+  abilities: function(){
+    let abilityArr = data.abilities;
+  for(let i = 0; i < abilityArr.length; i++){
+    let abName = abilityArr[i].ability.name;
+    let newArr = [abName];
+    return newArr;
+  }
+  },
+  statistics: [1,2,3,4]
+};
+console.log(pokemonData);
+
+window.localStorage.setItem("pokemonData", JSON.stringify(pokemonData));
+console.log(localStorage);
+
+console.log(JSON.parse(localStorage.getItem("pokemonData")));
 
 }
 else {
